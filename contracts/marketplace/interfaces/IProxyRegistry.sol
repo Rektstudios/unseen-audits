a14f59aa2d977b83ed5f753f17c971caffd04d8a
dcapinator <edmond@rektstudios.com>
2024-08-08 17:22:18 +0300

    fix: update globalmaker imports

--- contracts/marketplace/interfaces/IProxyRegistry.sol
+++ contracts/marketplace/interfaces/IProxyRegistry.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import { AuthenticatedProxy } from "../registry/AuthenticatedProxy.sol";
-
 /**
  * @title  IProxyRegistry
  * @author decapitator (0xdecapitator.eth)
  */
 interface IProxyRegistry {
 
-    function registerProxy() external returns (AuthenticatedProxy);
+    function registerProxy() external;
 }
